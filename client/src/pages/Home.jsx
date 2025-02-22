33c5f421f1553c585fb1d491f61054a62f2f76c2
Ayush Pyakurel <ayushpyakurel449@gmail.com>
2025-02-23 05:21:41 +0545

    Added Home page text and animations resolve tailwind css error

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import { motion, AnimatePresence } from 'framer-motion';
 
 import { useSnapshot } from 'valtio';
 
+import state from '../store';
+
 import {
   headContainerAnimation,
   headContentAnimation,
@@ -10,7 +12,42 @@ import {
 } from '../config/motion';
 
 const Home = () => {
-  return <div>Home</div>;
+  const snap = useSnapshot(state);
+
+  return (
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className='home ' {...slideAnimation('left')}>
+          <motion.header {...slideAnimation('down')}>
+            <img
+              src='./threejs.png'
+              alt='threejs logo'
+              className='w-8 h-8 object-contain'
+            />
+          </motion.header>
+
+          <motion.div className='home-content' {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className='head-text'>
+                Let's <br className='xl:block hidden' /> DO IT.
+              </h1>
+            </motion.div>
+            <motion.div
+              {...headContentAnimation}
+              className='flex flex-col gap-5'
+            >
+              <p className='max-w-md font-normal text-grey-600 text-base'>
+                Create your unique and exclusive shirt with our brand-new 3D
+                customization tool.{' '}
+                <strong>unleash your imaginatination</strong> and define your
+                own style
+              </p>
+            </motion.div>
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
+  );
 };
 
 export default Home;
