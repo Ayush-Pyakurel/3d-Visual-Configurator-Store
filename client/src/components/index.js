512468bd273bd7d36043c11a120e21f7732d4b84
Ayush Pyakurel <ayushpyakurel449@gmail.com>
2025-02-23 06:41:15 +0545

    Added the tabs for filter and functionality added the assets folder for icons created component default template

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,7 @@
 import CustomButton from './CustomButton';
+import Tab from './Tab';
+import AiPicker from './AiPicker';
+import ColorPicker from './ColorPicker';
+import FilePicker from './FilePicker';
 
-export { CustomButton };
+export { CustomButton, FilePicker, Tab, AiPicker, ColorPicker };
