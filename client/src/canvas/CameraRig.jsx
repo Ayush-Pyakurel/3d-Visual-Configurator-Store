c5c476e7e01938f240746dd71ab7984c343785b1
Ayush Pyakurel <ayushpyakurel449@gmail.com>
2025-02-24 09:33:19 +0545

    Added the tshirt logo, texture Decals resolve the error where tshirt dont get saved added animation on the tshirt mesh

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -1,38 +1,35 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-const-assign */
-/* eslint-disable no-unused-vars */
+
+import { useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { easing } from 'maath';
 import { useSnapshot } from 'valtio';
 
 import state from '../store';
-import { useRef } from 'react';
 
 const CameraRig = ({ children }) => {
-  const snap = useSnapshot(state);
-
   const group = useRef();
+  const snap = useSnapshot(state);
 
   useFrame((state, delta) => {
     const isBreakpoint = window.innerWidth <= 1260;
     const isMobile = window.innerWidth <= 600;
 
     // set the initial position of the model
-    const targetPosition = [-0.4, 0, 2];
-
+    let targetPosition = [-0.4, 0, 2];
     if (snap.intro) {
       if (isBreakpoint) targetPosition = [0, 0, 2];
-      if (isMobile) targetPosition = [0, -0.2, 2.5];
+      if (isMobile) targetPosition = [0, 0.2, 2.5];
     } else {
-      if (isMobile) {
-        if (isMobile) targetPosition = [0, 0, 2.5];
-        else targetPosition = [0, 0, 2];
-      }
+      if (isMobile) targetPosition = [0, 0, 2.5];
+      else targetPosition = [0, 0, 2];
     }
 
+    // set model camera position
     easing.damp3(state.camera.position, targetPosition, 0.25, delta);
 
-    //set the model rotation smoothly
+    // set the model rotation smoothly
     easing.dampE(
       group.current.rotation,
       [state.pointer.y / 10, -state.pointer.x / 5, 0],
--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -1,9 +1,11 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/no-unknown-property */
 import { easing } from 'maath';
 import { useSnapshot } from 'valtio';
 import { Decal, useGLTF, useTexture } from '@react-three/drei';
 
 import state from '../store';
+import { useFrame } from '@react-three/fiber';
 
 const Shirt = () => {
   const snap = useSnapshot(state);
@@ -13,15 +15,42 @@ const Shirt = () => {
   const logoTexture = useTexture(snap.logoDecal);
   const fullTexture = useTexture(snap.fullDecal);
 
+  useFrame(({ state, delta }) =>
+    easing.dampC(materials.lambert1.color, snap.color, 0.25, delta)
+  );
+
+  // fix the problem of tshirt not updating on saves
+  const stateString = JSON.stringify(snap);
+
   return (
-    <group>
+    <group key={stateString}>
       <mesh
         castShadow
         geometry={nodes.T_Shirt_male.geometry}
-        material={materials.lambart1}
+        material={materials.lambert1}
         material-roughness={1}
         dispose={null}
-      ></mesh>
+      >
+        {snap.isFullTexture && (
+          <Decal
+            position={[0, 0, 0]}
+            rotation={[0, 0, 0]}
+            scale={1}
+            map={fullTexture}
+          />
+        )}
+
+        {snap.isLogoTexture && (
+          <Decal
+            position={[0, 0.04, 0.15]}
+            rotation={[0, 0, 0]}
+            scale={0.15}
+            map={logoTexture}
+            depthTest={false}
+            depthWrite={true}
+          />
+        )}
+      </mesh>
     </group>
   );
 };
