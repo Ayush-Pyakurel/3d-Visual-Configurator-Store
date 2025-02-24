fab438ae7324bf5d81c9f9c45f5330ff13f8036c
Ayush Pyakurel <ayushpyakurel449@gmail.com>
2025-02-24 15:49:13 +0545

    Added event on editor tab

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -21,20 +21,53 @@ import state from '../store';
 const Customizer = () => {
   const snap = useSnapshot(state);
 
+  const [file, setFile] = useState('');
+  const [prompt, setPrompt] = useState('');
+  const [generatingImg, setGeneratingImg] = useState(false);
+
+  const [activeEditorTab, setActiveEditorTab] = useState('');
+  const [activeFilterTab, setActiveFilterTab] = useState({
+    logoShirt: true,
+    stylishShirt: false,
+  });
+
+  // show the tab content depending on the activeTab
+  const generateTabContent = () => {
+    switch (activeEditorTab) {
+      case 'colorpicker':
+        return <ColorPicker />;
+      case 'filepicker':
+        return <FilePicker />;
+      case 'aipicker':
+        return <AiPicker />;
+
+      default:
+        return null;
+    }
+  };
+
   return (
     <AnimatePresence>
       {!snap.intro && (
         <>
           <motion.div
             key='custom'
-            className='absolute top-0 left-0 z-10'
+            className='absolute top-0 left-0 z-10 '
             {...slideAnimation('left')}
           >
-            <div className='flex items-center min-h-screen'>
-              <div className='editortabs-container tabs'>
+            <div className='flex items-center min-h-screen '>
+              <div className='editortabs-container tabs '>
                 {EditorTabs.map((tab) => (
-                  <Tab key={tab.name} tab={tab} handleClick={() => {}} />
+                  <Tab
+                    key={tab.name}
+                    tab={tab}
+                    handleClick={() => {
+                      setActiveEditorTab(tab.name);
+                    }}
+                  />
                 ))}
+
+                {generateTabContent()}
               </div>
             </div>
           </motion.div>
@@ -60,7 +93,7 @@ const Customizer = () => {
                 key={tab.name}
                 tab={tab}
                 isFilterTab
-                isActive=''
+                isActiveTab=''
                 handleClick={() => {}}
               />
             ))}
